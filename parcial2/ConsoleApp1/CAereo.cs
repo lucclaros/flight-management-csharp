73674866a272b16a6135835402dead5b984b7dab
lucclaros <135435931+lucclaros@users.noreply.github.com>
2025-08-29 17:13:00 -0300

    Update CAereo.cs

--- parcial2/ConsoleApp1/CAereo.cs
+++ parcial2/ConsoleApp1/CAereo.cs
@@ -4,14 +4,14 @@ namespace EmpresaAterrizar
     class CAereo : CViaje
     {
         private static float impuesto;
-        private string tipo_de_vuelo; // esto deberia ser una clase!
+        private string tipo_de_vuelo; 
 
         public static void SetImpuesto(float imp)
         {
-            // this.impuesto = imp;
+            
             CAereo.impuesto = imp;
         }
-        public CAereo(string cod, string orig, string dest, float pre, string tipo): base(cod, orig, dest) // como no puedo acceder desde aca al constructor de cviaje, lo mando por : base
+        public CAereo(string cod, string orig, string dest, float pre, string tipo): base(cod, orig, dest) 
         {
             this.PrecioViaje = pre;
             this.tipo_de_vuelo = tipo;
@@ -19,8 +19,9 @@ namespace EmpresaAterrizar
 
         public override string darDatos()
         {
-            return base.darDatos() + " - Impuesto de: " + CAereo.impuesto.ToString() + " - Tipo de vuelo: " + this.tipo_de_vuelo; // NO OLVIDARME DEL TOSTRING!!!!!!!!!!!!!!!!!!!!!
+            return base.darDatos() + " - Impuesto de: " + CAereo.impuesto.ToString() + " - Tipo de vuelo: " + this.tipo_de_vuelo; 
         } 
 
     }
 }
+
